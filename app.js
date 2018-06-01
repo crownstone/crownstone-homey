2b426f02b45402f96ab77e1e3d02f8712644bd33
Anne van Rossum <anne@crownstone.rocks>
2018-06-01 18:25:30 +0200

    Add GUI elements to connect to Crownstone cloud.

--- app.js
+++ app.js
@@ -6,23 +6,35 @@ const BluenetLib = require('./ble/index');
 class MyApp extends Homey.App {
 
 
-	onInit() {
-		this.log('Crownstone is running...');
-		this.log('BluenetLib')
+  onInit() {
+    this.log('Crownstone is running...');
+    this.log('BluenetLib')
 
     this.bluenet = new BluenetLib.Bluenet()
 
-    this.startBle()
-	}
+    this.email = Homey.ManagerSettings.get("email");
+    this.password = Homey.ManagerSettings.get("password");
+    this.sphere = Homey.ManagerSettings.get("sphere");
+    
+    let userData = {
+      "email": this.email,
+      "password": this.password,
+      "sphereId": this.sphere
+    }
 
-	startBle() {
+    this.bluenet.linkCloud(userData);
+
+    this.startBle()
+  }
+
+  startBle() {
     let BleManager = Homey.ManagerBLE;
     console.log("Start Scanning")
     BleManager.discover([], 5*1000, (err, advertisements) => {
       console.log('err)',err)
       console.log('advertisements)')
       advertisements.forEach((adv) => {
-        console.log(adv, adv.rssi)
+	console.log(adv, adv.rssi)
       })
     })
   }
