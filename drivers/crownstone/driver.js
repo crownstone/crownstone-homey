2f86e2f294d44d52efcdf36262fc7c571015c75b
MJKoedam <martjankoedam42@gmail.com>
2020-10-13 15:01:09 +0200

    added a function to determine the current location of the user and thus the sphere with all the devices in it

--- drivers/crownstone/driver.js
+++ drivers/crownstone/driver.js
@@ -26,56 +26,49 @@ class CrownstoneDriver extends Homey.Driver {
     }
 
     onPairListDevices( data, callback ) {
-
-        this.log("Discover Crownstones in cloud");
-        
-        let devices = [];
-
-        this.log("Get current sphere");
-
-        this.cloudAPI.getUserLocation()
-            .then((userLocations) => {
-                let sphereId = null;
-                if (userLocations && Array.isArray(userLocations) && userLocations.length > 0) {
-                    let spheres = userLocations[0].inSpheres;
-                    // Just assume one sphere per physical location now
-                    if (spheres && Array.isArray(spheres) && spheres.length > 0) {
-                        sphereId = spheres[0].sphereId;
-                        this.log("Smartphone in sphere", sphereId);
-                    } else {
-                        this.log("No sphere found");
-                    }
-                } else {
-                    this.log("No array of user locations")
-                }
-                return sphereId; 
-            })
-            .then((sphereId) => {
-                if (sphereId) {
-                    return this.cloudAPI.forSphere(sphereId).getStonesInSphere();
-                } else {
-                    return Promise.reject( new Error('Cannot find the current sphere...') );
-                }
-            })
-            .then((stones) => {
-                if (stones && Array.isArray(stones)) {
-                    stones.forEach((stone) => {
-                        if (stone.type === 'PLUG' || stone.type === 'BUILTIN') {
-                            let device = {};
-                            device["name"] = stone.name;
-                            device["data"] = stone;
-                            devices.push(device);
-                        }
-                    })
-                }
-            })
-            .then(() => {
-                callback( null, devices);
-            })
-            .catch((err) => {
-                this.log("Error: No stones", err);
-            })
+        this.log('Start discovering Crownstones in cloud');
+        getCurrentLocation(this.cloud, this.getDevices(), function(devices){
+            callback(null, devices);
+        }).catch((e) => { console.log('There was a problem looking for Crownstones in the cloud:', e); });
     }
+}
+
+async function getCurrentLocation(cloud, existingDevices, callback){
+    let devices = [];
+    cloud.setAccessToken(accessToken);
+    let userReference = await cloud.me();
+    let userLocation = await userReference.currentLocation();
+    if(userLocation.length > 0){
+        if(userLocation[0]['inSpheres'].length > 0){
+            let sphereId = userLocation[0]['inSpheres'][0]['sphereId'];	// get sphere closest to the user
+            let allCs = await cloud.sphere(sphereId).crownstones();
+            for (let i = 0; i < allCs.length; i++){
+                console.log('Crownstone ' + i + ' with ID: ' + allCs[i].id + ' and name: ' + allCs[i].name);
+
+                /**
+                 * The next piece code is temporarily required to prevent a bug on the Homey which lets you add devices multiple times.
+                 */
+                let duplicate = false;
+                for (let j = 0; j < existingDevices.length; j++){
+                    if(existingDevices[j].getData().id == allCs[i].id){
+                        duplicate = true;
+                        console.log(allCs[i].name + ' already exists!');
+                    }
+                }
+                if(!duplicate){
+                    let device = {};
+                    device['name'] = allCs[i].name;
+                    device['data'] = allCs[i];
+                    devices.push(device);
+                }
+            }
+        } else {
+            this.log('No sphere found')
+        }
+    } else {
+        this.log('No userlocation found');
+    }
+    callback(devices);
 }
 
 module.exports = CrownstoneDriver;
