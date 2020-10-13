808a64c80df4d4e09f2b0e28657719c79752f8a3
MJKoedam <martjankoedam42@gmail.com>
2020-10-13 14:14:09 +0200

    added a function to obtain the accesstoken for the cloud for the drivers and devices to use in the future

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const Homey = require('homey');
 let cloudLib = require('crownstone-cloud')
 let cloud = new cloudLib.CrownstoneCloud();
+let accessToken;
 
 /**
  * This class gets the data from the form shown to the user when the latter install the Crownstone app. There are
@@ -17,12 +18,32 @@ class CrownstoneApp extends Homey.App {
     this.password = Homey.ManagerSettings.get('password');
     loginToCloud(this.email, this.password).catch((e) => { console.log('There was a problem making a connection with the cloud:', e); });
   }
+  // [todo] simplify code
+  getUserToken(callback) {
+    retrieveAccessToken(function(){
+      callback(accessToken);
+    });
+  }
+  getCloud(){
+    return cloud;
+  }
 }
 
 // Make a connection with the cloud and obtain the userdata
 async function loginToCloud(email, password){
   await cloud.login(email, password);
   let userData = await cloud.me();
+  accessToken = userData.rest.tokenStore.accessToken;
+}
+
+// return accesstoken as soon as it is obtained
+function retrieveAccessToken(callback){
+  if(typeof accessToken !== 'undefined') {
+    callback();
+  } else {
+    setTimeout(function(){
+      retrieveAccessToken(callback);}, 50);
+  }
 }
 
 module.exports = CrownstoneApp;
