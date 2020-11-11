656515387cbbb3c872a8cfd13eda6a0ac0d77a88
MJKoedam <martjankoedam42@gmail.com>
2020-11-11 15:26:20 +0100

    improved code syntax and style

--- app.js
+++ app.js
@@ -1,64 +1,56 @@
-'use strict';
-
 const Homey = require('homey');
-let cloudLib = require('crownstone-cloud')
-const sseLib = require('crownstone-sse')
-let cloud = new cloudLib.CrownstoneCloud();
-let sse = new sseLib.CrownstoneSSE();
+const cloudLib = require('crownstone-cloud');
+const sseLib = require('crownstone-sse');
+
+const cloud = new cloudLib.CrownstoneCloud();
+const sse = new sseLib.CrownstoneSSE();
 let sphereId;
 let inLocationName;
 let inLocationId;
 
-let presenceTrigger = new Homey.FlowCardTrigger('user_enters_room');
-let presenceCondition = new Homey.FlowCardCondition('user_presence');
+const presenceTrigger = new Homey.FlowCardTrigger('user_enters_room');
+const presenceCondition = new Homey.FlowCardCondition('user_presence');
 
 /**
  * This code runs when a trigger has been fired. If the room name and id are equal, the flow will run.
  */
-presenceTrigger.register().registerRunListener((args, state) => {
-  return Promise.resolve(args.rooms.name === state.name && args.rooms.id === state.id);
-})
+presenceTrigger.register().registerRunListener((args, state) => Promise.resolve(args.rooms.name === state.name && args.rooms.id === state.id));
 
 /**
  * This code runs after a trigger has been fired and a condition-card is configured in the flow.
  * If the room name and id are equal to the name and id from the room the user is currently in, the flow will run.
  */
-presenceCondition.register().registerRunListener(async( args, state ) => {
-  await getCurrentLocation(function(){}).catch((e) => { console.log('There was a problem getting the user location:', e); });
+presenceCondition.register().registerRunListener(async (args) => {
+  await getCurrentLocation(() => {}).catch((e) => { console.log('There was a problem getting the user location:', e); });
   if (typeof inLocationId !== 'undefined' && typeof inLocationName !== 'undefined') {
     return Promise.resolve(args.rooms.id === inLocationId && args.rooms.name === inLocationName);
   }
   return false;
-})
+});
 
 /**
  * This code runs when a flow is being constructed for a trigger-card, and a room should be selected.
  * This code returns a list of rooms in a sphere which is shown to the user.
  */
-presenceTrigger.getArgument('rooms').registerAutocompleteListener(( query, args ) => {
-  return Promise.resolve(getRooms().catch((e) => { console.log('There was a problem obtaining the rooms:', e);}));
-})
+presenceTrigger.getArgument('rooms').registerAutocompleteListener(() => Promise.resolve(getRooms().catch((e) => { console.log('There was a problem obtaining the rooms:', e); })));
 
 /**
  * This code runs when a flow is being constructed for a condition-card, and a room should be selected.
  * This code returns a list of rooms in a sphere.
  */
-presenceCondition.getArgument('rooms').registerAutocompleteListener(( query, args ) => {
-  return Promise.resolve(getRooms().catch((e) => { console.log('There was a problem obtaining the rooms:', e);}));
-})
+presenceCondition.getArgument('rooms').registerAutocompleteListener(() => Promise.resolve(getRooms().catch((e) => { console.log('There was a problem obtaining the rooms:', e); })));
 
 /**
  * This class gets the data from the form shown to the user when the latter install the Crownstone app. There are
- * only two fields, email and password. This is used to retrieve all information from the Crownstone cloud. 
+ * only two fields, email and password. This is used to retrieve all information from the Crownstone cloud.
  */
 class CrownstoneApp extends Homey.App {
-
   /**
    * This method is called when the App is initialized.
    * The email and password for the Crownstone Cloud from the user will be obtained using the data from the form.
    * Instances of the flowcard triggers and conditions are inited.
    */
-  onInit(){
+  onInit() {
     this.log(`App ${Homey.app.manifest.name.en} is running...`);
     this.email = Homey.ManagerSettings.get('email');
     this.password = Homey.ManagerSettings.get('password');
@@ -68,7 +60,7 @@ class CrownstoneApp extends Homey.App {
     /**
      * This function will fire when a user changed the credentials in the settings-page.
      */
-    Homey.ManagerSettings.on('set', function (){
+    Homey.ManagerSettings.on('set', function () {
       this.email = Homey.ManagerSettings.get('email');
       this.password = Homey.ManagerSettings.get('password');
       loginToCloud(this.email, this.password).catch((e) => { console.log('There was a problem making a connection with the cloud:', e); });
@@ -79,8 +71,8 @@ class CrownstoneApp extends Homey.App {
   /**
    * This method will call the getCurrentLocation function and returns the sphere ID with a callback.
    */
-  getLocation(callback){
-    getCurrentLocation(function(){
+  getLocation(callback) {
+    getCurrentLocation(() => {
       callback(cloud, sphereId);
     }).catch((e) => { console.log('There was a problem getting the user location:', e); });
   }
@@ -88,7 +80,7 @@ class CrownstoneApp extends Homey.App {
   /**
    * This method will return the instance of the cloud.
    */
-  getCloud(){
+  getCloud() {
     return cloud;
   }
 }
@@ -96,7 +88,7 @@ class CrownstoneApp extends Homey.App {
 /**
  * This function will make a connection with the cloud and obtain the userdata.
  */
-async function loginToCloud(email, password){
+async function loginToCloud(email, password) {
   await cloud.login(email, password);
 }
 
@@ -104,9 +96,9 @@ async function loginToCloud(email, password){
  * This function will stop all running eventHandlers, in case a user enters other credentials,
  * make a new connection with the sse-server and starts the eventHandler.
  */
-async function loginToEventServer(email, password){
+async function loginToEventServer(email, password) {
   await sse.stop();
-  await sse.login(email , password);
+  await sse.login(email, password);
   await sse.start(eventHandler);
 }
 
@@ -114,24 +106,24 @@ async function loginToEventServer(email, password){
  * The eventHandler receives events from the sse-server and fires the presenceTrigger when a user enters a room.
  */
 let eventHandler = (data) => {
-  if(data.type === 'presence' && data.subType === 'enterLocation'){
-    let state = {'name': data.location.name, 'id': data.location.id}
-    presenceTrigger.trigger(null, state).then(this.log).catch(this.error)
+  if (data.type === 'presence' && data.subType === 'enterLocation') {
+    const state = { name: data.location.name, id: data.location.id };
+    presenceTrigger.trigger(null, state).then(this.log).catch(this.error);
   }
-}
+};
 
 /**
  * This function will obtain the sphere and, if available, the room where the user is currently located.
  */
-async function getCurrentLocation(callback){
-  let userReference = await cloud.me();
-  let userLocation = await userReference.currentLocation();
+async function getCurrentLocation(callback) {
+  const userReference = await cloud.me();
+  const userLocation = await userReference.currentLocation();
   if (userLocation.length > 0) {
-    let spheres = await cloud.spheres();
+    const spheres = await cloud.spheres();
     if (spheres.length > 0) {
-      sphereId = await userLocation[0]['inSpheres'][0]['sphereId'];
-      inLocationName = await userLocation[0]['inSpheres'][0]['inLocation']['locationName'];
-      inLocationId = await userLocation[0]['inSpheres'][0]['inLocation']['locationId'];
+      sphereId = userLocation[0].inSpheres[0].sphereId;
+      inLocationName = await userLocation[0].inSpheres[0].inLocation.locationName;
+      inLocationId = await userLocation[0].inSpheres[0].inLocation.locationId;
       callback();
     } else {
       console.log('Unable to find sphere');
@@ -144,27 +136,27 @@ async function getCurrentLocation(callback){
 /**
  * This function obtains all the rooms of the sphere where the user is currently located in.
  */
-async function getRooms(){
-  await getCurrentLocation(function(){}).catch((e) => { console.log('There was a problem getting the user location:', e); });
-  let rooms = await cloud.sphere(sphereId).locations();
-  if(rooms.length > 0){
+async function getRooms() {
+  await getCurrentLocation(() => {}).catch((e) => { console.log('There was a problem getting the user location:', e); });
+  const rooms = await cloud.sphere(sphereId).locations();
+  if (rooms.length > 0) {
     return listRooms(rooms);
-  } else {
-    console.log('Unable to find any rooms');
   }
+  console.log('Unable to find any rooms');
+  return null;
 }
 
 /**
  * This function returns a json list with all the rooms in the sphere.
  * [todo:] add custom icons
  */
-function listRooms(rooms){
-  let roomList = [];
-  for(let i = 0; i < rooms.length; i++){
-    let room = {
-      'name': rooms[i].name,
-      'id': rooms[i].id
-    }
+function listRooms(rooms) {
+  const roomList = [];
+  for (let i = 0; i < rooms.length; i++) {
+    const room = {
+      name: rooms[i].name,
+      id: rooms[i].id,
+    };
     roomList.push(room);
   }
   return roomList;
--- drivers/crownstone/device.js
+++ drivers/crownstone/device.js
@@ -1,56 +1,53 @@
-'use strict';
-
 const Homey = require('homey');
 
 /**
  * Current we have implemented this through the BLE chip on board of the Homey. This has the advantage that we can
  * directly send a command to the Crownstone itself. Henceforth, it should be faster than a round-trip through the
- * cloud. However, in this way it is only possible to set up a connection to a Crownstone that is in the 
+ * cloud. However, in this way it is only possible to set up a connection to a Crownstone that is in the
  * neighbourhood of the Homey. The Crownstone app knows how to send messages into the mesh. The Homey does not. It
  * only is able to send a request to a device that is broadcasting BLE advertisements in its neighbourhood.
  *
- * It might make sense to have a toggle that defines this behavior. It can define if we should go through the 
+ * It might make sense to have a toggle that defines this behavior. It can define if we should go through the
  * Crownstone app (but then the particular phone has to be present) or directly with the Crownstones in its vicinity.
  */
 class CrownstoneDevice extends Homey.Device {
-
-    /**
+  /**
      * This method is called when the Device is initialized. It does not necessarily do any scanning itself.
      * It will obtain the cloud instance and the access token.
      * */
-    onInit(){
-        this.log(this.getName() + ' has been inited');
-        this.log('Name:', this.getName());
-        this.log('Class:', this.getClass());
-        this.cloud = Homey.app.getCloud();
-        this.registerCapabilityListener('onoff', this.onCapabilityOnoff.bind(this));
-    }
+  onInit() {
+    this.log(`${this.getName()} has been inited`);
+    this.log('Name:', this.getName());
+    this.log('Class:', this.getClass());
+    this.cloud = Homey.app.getCloud();
+    this.registerCapabilityListener('onoff', this.onCapabilityOnoff.bind(this));
+  }
 
-    /**
+  /**
      * Called when the device has requested a state change (turned on or off).
      */
-    async onCapabilityOnoff(value, opts){
-        if (value){
-            await this.cloud.crownstone(this.getData().id).turnOn()
-        } else if (!value){
-            await this.cloud.crownstone(this.getData().id).turnOff()
-        }
+  async onCapabilityOnoff(value) {
+    if (value) {
+      await this.cloud.crownstone(this.getData().id).turnOn();
+    } else if (!value) {
+      await this.cloud.crownstone(this.getData().id).turnOff();
     }
+  }
 
-    /**
+  /**
      * this method is called when the Device is added.
      * */
-    onAdded(){
-        this.log(this.getName() + ' has been added.');
-        // todo register a capability listener for dimming if the Crownstone can be dimmed (use this.getData())
-    }
+  onAdded() {
+    this.log(`${this.getName()} has been added.`);
+    // todo register a capability listener for dimming if the Crownstone can be dimmed (use this.getData())
+  }
 
-    /**
+  /**
      * this method is called when the Device is deleted.
      * */
-    onDeleted(){
-        this.log(this.getName() + ' has been deleted.');
-    }
+  onDeleted() {
+    this.log(`${this.getName()} has been deleted.`);
+  }
 }
 
 module.exports = CrownstoneDevice;
