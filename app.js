16813c98ac07a2ecf7f888fa241a721012f445bf
MJKoedam <martjankoedam42@gmail.com>
2020-11-17 17:19:46 +0100

    Added the option to use users in a card. - made variable names more clear. - return empty lists instead of null. - changed  to a normal function. - splitted lines of code to limit the length of one line. - the users and their locations are stored in a variable in the app. - a function is added to update the users and their locations for when the app boots and checks every 10 minutes in case an event was missed. - changed 'getCurrentLocation' to 'getSphereId' to fit better with it's functionality. - added presentPeople-function to check all the users and their locations.
    todo:
    - presentPeople-check every x minutes in case of missed events.
    - simplify the loginproces for the cloud and eventHandler.
    - add filter with delay for leaving a room.
    - remove the debuglogs.

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const sseLib = require('crownstone-sse');
 
 const cloud = new cloudLib.CrownstoneCloud();
 const sse = new sseLib.CrownstoneSSE();
+
 let sphereId;
-let inLocationName;
-let inLocationId;
+let userLocations = [];
 
 const presenceTrigger = new Homey.FlowCardTrigger('user_enters_room');
 const presenceCondition = new Homey.FlowCardCondition('user_presence');
@@ -14,7 +14,9 @@ const presenceCondition = new Homey.FlowCardCondition('user_presence');
 /**
  * This code runs when a trigger has been fired. If the room name and id are equal, the flow will run.
  */
-presenceTrigger.register().registerRunListener((args, state) => Promise.resolve((args.rooms.name === state.name && args.rooms.id === state.roomId) && (args.users.id === state.userId || args.users.id === 'default')));
+presenceTrigger.register().registerRunListener((args, state) =>
+    Promise.resolve(args.rooms.id === state.locationId && args.users.id === state.userId || args.users.id === 'default')
+);
 
 /**
  * This code runs after a trigger has been fired and a condition-card is configured in the flow.
@@ -22,40 +24,54 @@ presenceTrigger.register().registerRunListener((args, state) => Promise.resolve(
  * [todo:] Check for users!!
  */
 presenceCondition.register().registerRunListener(async (args) => {
-  await getCurrentLocation(() => {}).catch((e) => { console.log('There was a problem getting the user location:', e); });
-  if (typeof inLocationId !== 'undefined' && typeof inLocationName !== 'undefined') {
-    return Promise.resolve((args.rooms.id === inLocationId && args.rooms.name === inLocationName) && (true));
+  let userInList = checkUserId(args.users.id);
+  if (userInList > -1) {
+    if (args.rooms.id === userLocations[userInList].locations[0]) {
+      console.log('return true');
+    } else {
+      console.log('return false');
+    }
+    return Promise.resolve(args.rooms.id === userLocations[userInList].locations[0]);
   }
   return false;
 });
-
-
 
 /**
  * This code runs when a flow is being constructed for a trigger-card, and a room should be selected.
  * This code returns a list of rooms in a sphere which is shown to the user.
  */
-presenceTrigger.getArgument('rooms').registerAutocompleteListener(() => Promise.resolve(getRooms().catch((e) => { console.log('There was a problem obtaining the rooms:', e); })));
+presenceTrigger.getArgument('rooms').registerAutocompleteListener(() =>
+    Promise.resolve(getRooms().catch((e) => { console.log('There was a problem obtaining the rooms:', e);
+})));
 
 /**
- * [todo:] documentation
+ * This code runs when a flow is being constructed for a trigger-card, and a user should be selected.
+ * This code returns a list of users in a sphere.
  */
-presenceTrigger.getArgument('users').registerAutocompleteListener(() => Promise.resolve(getUsers().catch((e) => { console.log('There was a problem obtaining the users:', e); })));
+presenceTrigger.getArgument('users').registerAutocompleteListener(() =>
+    Promise.resolve(getUsers().catch((e) => { console.log('There was a problem obtaining the users:', e);
+})));
 
 /**
  * This code runs when a flow is being constructed for a condition-card, and a room should be selected.
  * This code returns a list of rooms in a sphere.
  */
-presenceCondition.getArgument('rooms').registerAutocompleteListener(() => Promise.resolve(getRooms().catch((e) => { console.log('There was a problem obtaining the rooms:', e); })));
+presenceCondition.getArgument('rooms').registerAutocompleteListener(() =>
+    Promise.resolve(getRooms().catch((e) => { console.log('There was a problem obtaining the rooms:', e);
+})));
 
 /**
- * [todo:] documentation
+ * This code runs when a flow is being constructed for a condition-card, and a user should be selected.
+ * This code returns a list of users in a sphere.
  */
-presenceCondition.getArgument('users').registerAutocompleteListener(() => Promise.resolve(getUsers().catch((e) => { console.log('There was a problem obtaining the users:', e); })));
+presenceCondition.getArgument('users').registerAutocompleteListener(() =>
+    Promise.resolve(getUsers().catch((e) => { console.log('There was a problem obtaining the users:', e);
+})));
 
 /**
- * This class gets the data from the form shown to the user when the latter install the Crownstone app. There are
- * only two fields, email and password. This is used to retrieve all information from the Crownstone cloud.
+ * This class gets the data from the form shown to the user when the latter install the Crownstone app.
+ * There are only two fields, email and password.
+ * This is used to retrieve all information from the Crownstone cloud.
  */
 class CrownstoneApp extends Homey.App {
   /**
@@ -67,8 +83,10 @@ class CrownstoneApp extends Homey.App {
     this.log(`App ${Homey.app.manifest.name.en} is running...`);
     this.email = Homey.ManagerSettings.get('email');
     this.password = Homey.ManagerSettings.get('password');
-    loginToCloud(this.email, this.password).catch((e) => { console.log('There was a problem making a connection with the cloud:', e); });
-    loginToEventServer(this.email, this.password).catch((e) => { console.log('There was a problem making a connection with the event server:', e); });
+    loginToCloud(this.email, this.password).catch((e) => {
+      console.log('There was a problem making a connection with the cloud:', e); });
+    loginToEventServer(this.email, this.password).catch((e) => {
+      console.log('There was a problem making a connection with the event server:', e); });
 
     /**
      * This function will fire when a user changed the credentials in the settings-page.
@@ -76,18 +94,20 @@ class CrownstoneApp extends Homey.App {
     Homey.ManagerSettings.on('set', function () {
       this.email = Homey.ManagerSettings.get('email');
       this.password = Homey.ManagerSettings.get('password');
-      loginToCloud(this.email, this.password).catch((e) => { console.log('There was a problem making a connection with the cloud:', e); });
-      loginToEventServer(this.email, this.password).catch((e) => { console.log('There was a problem making a connection with the event server:', e); });
+      loginToCloud(this.email, this.password).catch((e) => {
+        console.log('There was a problem making a connection with the cloud:', e); });
+      loginToEventServer(this.email, this.password).catch((e) => {
+        console.log('There was a problem making a connection with the event server:', e); });
     });
   }
 
   /**
-   * This method will call the getCurrentLocation function and returns the sphere ID with a callback.
+   * This method will call the getSphereId function and returns the sphere ID with a callback.
    */
   getLocation(callback) {
-    getCurrentLocation(() => {
+    getSphereId(() => {
       callback(cloud, sphereId);
-    }).catch((e) => { console.log('There was a problem getting the user location:', e); });
+    }).catch((e) => { console.log('There was a problem getting the sphere Id:', e); });
   }
 
   /**
@@ -103,6 +123,8 @@ class CrownstoneApp extends Homey.App {
  */
 async function loginToCloud(email, password) {
   await cloud.login(email, password);
+  await getPresentPeople(() => {}).catch((e) => {
+    console.log('There was a problem getting the locations of the users:', e); });
 }
 
 /**
@@ -116,27 +138,113 @@ async function loginToEventServer(email, password) {
 }
 
 /**
- * The eventHandler receives events from the sse-server and fires the presenceTrigger when a user enters a room.
+ * The eventHandler receives events from the sse-server and fires the runTrigger-function
+ * when a user enters or leaves a room.
  */
 let eventHandler = (data) => {
   if (data.type === 'presence' && data.subType === 'enterLocation') {
-    const state = { name: data.location.name, roomId: data.location.id, userId: data.user.id };
-    presenceTrigger.trigger(null, state).then(this.log).catch(this.error);
+    console.log(data.user.name + ' enters ' + data.location.name);
+    runTrigger(data, true).catch((e) => {
+      console.log('There was a problem firing the trigger:', e); });
+  }
+  if (data.type === 'presence' && data.subType === 'exitLocation') {
+    console.log(data.user.name + ' exits ' + data.location.name);
+    runTrigger(data, false).catch((e) => {
+      console.log('There was a problem firing the trigger:', e); });
   }
 };
 
 /**
+ * This function will update the userLocations-list and will fire the trigger after it is complete.
+ */
+async function runTrigger(data, entersRoom) {
+  const state = { userId: data.user.id, locationId: data.location.id };
+  await updateUserLocation(entersRoom, data.user.id, data.location.id);
+  console.log('TRIGGER!');
+  if (entersRoom) { presenceTrigger.trigger(null, state).then(this.log).catch(this.error); }
+}
+
+/**
+ * This function will update the userLocations-list by using events.
+ * If an ID is missing or is in the wrong place,
+ * the getPresentPeople-function will be called to refresh the list.
+ */
+async function updateUserLocation(entersRoom, userId, location) {
+  console.log('START UPDATE');
+  let userInList = checkUserId(userId);
+  if (entersRoom) {
+    console.log('checkpoint 1');
+    console.log(userInList);
+    if (userInList < 0) {
+      console.log('checkpoint 2');
+      //user not in list yet
+      const userLocation = {
+        userId: userId,
+        location: location,
+      };
+      console.log('add this user to the list since he entered the room');
+      userLocations.push(userLocation);
+    } else {
+      if (userLocations[userInList].locations[0] === location) {
+        getPresentPeople(() => {}).catch((e) => {
+          console.log('There was a problem getting the locations of the users:', e); });
+      } else {
+        //user is already in list.. so change location:
+        console.log('change location to: ' + location);
+        userLocations[userInList].locations[0] = location;
+      }
+    }
+  } else if (!entersRoom) {
+    //user exits location
+    if (userInList > -1) {
+      //if the user is already in the list
+      if (userLocations[userInList].locations[0] !== location) {
+        //and the location of the user is not the same as the location the user just 'left'..
+        console.log(userLocations);
+        return;
+      }
+    }
+    getPresentPeople(() => {}).catch((e) => {
+      console.log('There was a problem getting the locations of the users:', e); });
+  }
+  console.log(userLocations);
+  console.log('END UPDATE');
+}
+
+/**
+ * This function will check if the userId is already defined in the list of userLocations.
+ */
+function checkUserId(userId) {
+  for (let i = 0; i < userLocations.length; i++) {
+    if (userLocations[i].userId === userId) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+/**
+ * This function will obtain all the users and their locations in the sphere.
+ */
+async function getPresentPeople() {
+  console.log('GETPRESENTPEOPLE()');
+  await getSphereId(() => {}).catch((e) => { console.log('There was a problem getting the sphere Id:', e); });
+  userLocations = await cloud.sphere(sphereId).presentPeople();
+  console.log(userLocations);
+}
+
+/**
  * This function will obtain the sphere and, if available, the room where the user is currently located.
  */
-async function getCurrentLocation(callback) {
+async function getSphereId(callback) {
   const userReference = await cloud.me();
   const userLocation = await userReference.currentLocation();
   if (userLocation.length > 0) {
     const spheres = await cloud.spheres();
     if (spheres.length > 0) {
       sphereId = userLocation[0].inSpheres[0].sphereId;
-      inLocationName = await userLocation[0].inSpheres[0].inLocation.locationName;
-      inLocationId = await userLocation[0].inSpheres[0].inLocation.locationId;
+      // getPresentPeople(() => {}).catch((e) => {
+      //   console.log('There was a problem getting the locations of the users:', e); });
       callback();
     } else {
       console.log('Unable to find sphere');
@@ -150,13 +258,14 @@ async function getCurrentLocation(callback) {
  * This function obtains all the rooms of the sphere where the user is currently located in.
  */
 async function getRooms() {
-  await getCurrentLocation(() => {}).catch((e) => { console.log('There was a problem getting the user location:', e); });
+  await getSphereId(() => {}).catch((e) => {
+    console.log('There was a problem getting the sphere Id:', e); });
   const rooms = await cloud.sphere(sphereId).locations();
   if (rooms.length > 0) {
     return listRooms(rooms);
   }
   console.log('Unable to find any rooms');
-  return null;
+  return [];
 }
 
 /**
@@ -176,16 +285,18 @@ function listRooms(rooms) {
 }
 
 /**
- * [todo:] documentation
+ * This function will ask for the sphere Id and return a list of all the users in the sphere.
  */
 async function getUsers(){
-  await getCurrentLocation(() => {}).catch((e) => { console.log('There was a problem getting the user location:', e); });
+  await getSphereId(() => {}).catch((e) => {
+    console.log('There was a problem getting the sphere Id:', e); });
   const users = await cloud.sphere(sphereId).users();
   return listUsers(users);
 }
 
 /**
- * [todo:] documentation
+ * This function returns a list of all the users in the sphere.
+ * A default user 'somebody' is added to use in a card which will be accepted with every user check.
  */
 function listUsers(users) {
   const userList = [];
@@ -194,26 +305,26 @@ function listUsers(users) {
     id: 'default',
   };
   userList.push(defaultUser);
-  addUserToList(users.admins);
-  addUserToList(users.members);
-  addUserToList(users.guests);
+  addUserToList(userList, users.admins);
+  addUserToList(userList, users.members);
+  addUserToList(userList, users.guests);
   if (userList.length > 1) {
     return userList;
   }
   console.log('Unable to find any users');
-  return null;
+  return [];
+}
 
-  /**
-   * [todo:] documentation
-   */
-  function addUserToList(object) {
-    for (let i = 0; i < object.length; i++) {
-      const user = {
-        name: object[i].firstName + ' ' + object[i].lastName,
-        id: object[i].id,
-      };
-      userList.push(user);
-    }
+/**
+ * This function pushes json objects of the users to a user list.
+ */
+function addUserToList(userList, users) {
+  for (let i = 0; i < users.length; i++) {
+    const user = {
+      name: users[i].firstName + ' ' + users[i].lastName,
+      id: users[i].id,
+    };
+    userList.push(user);
   }
 }
 
